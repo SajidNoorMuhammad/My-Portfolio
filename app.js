fda9a1d184bde79e94b4f7d8deac47bfa79a9cde
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-05-26 21:23:23 +0500

    create a function.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var slideIndex = 0;
 var slides = document.querySelectorAll('#slideshow img');
 
-// function showSlides() {
-// slideIndex++;
-// if (slideIndex >= slides.length) {
-// slideIndex = 0;
-// }
+function showSlides() {
+slideIndex++;
+if (slideIndex >= slides.length) {
+slideIndex = 0;
+}
 
-// for (var i = 0; i < slides.length; i++) {
-// slides[i].classList.remove('active');
-// }
+for (var i = 0; i < slides.length; i++) {
+slides[i].classList.remove('active');
+}
 
-// slides[slideIndex].classList.add('active');
-// }
+slides[slideIndex].classList.add('active');
+}
 
 setInterval(showSlides, 2000);
